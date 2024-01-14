579f7b37133d37f692e93d079e89038f24729f46
Darina Khalina <darina.khalina@namecheap.com>
2024-01-14 15:29:53 +0200

    feat(hw3): add task1

---
+++ task1.py
@@ -0,0 +1,42 @@
+import argparse
+from pathlib import Path
+import shutil
+
+
+def parse_argv():
+    parser = argparse.ArgumentParser(description="Copy and sort files.")
+    parser.add_argument(
+        "-s", "--source", type=Path, required=True, help="Source directory path."
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=Path,
+        default=Path("output"),
+        help="Output directory path.",
+    )
+    return parser.parse_args()
+
+
+def recursive_copy_and_sort(source: Path, output: Path):
+    try:
+        for el in source.iterdir():
+            if el.is_dir():
+                recursive_copy_and_sort(el, output)
+            else:
+                extension = el.suffix[1:]
+                sub_dir = output / extension
+                sub_dir.mkdir(exist_ok=True, parents=True)
+                shutil.copy(el, sub_dir)
+                print(f"File '{el.name}' copied to '{sub_dir}'")
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+def main():
+    args = parse_argv()
+    recursive_copy_and_sort(args.source, args.output)
+
+
+if __name__ == "__main__":
+    main()
